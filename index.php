0f49cb56bdd9019518bd90e991063dc7189a2089
Aprianto WPJ <apriantowpj@gmail.com>
2021-05-17 18:20:10 +0700

    add welcome page

---
+++ index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html>
+<meta charset="UTF-8">
+<meta name="description" content="Project who made for cool people">
+<meta name="keywords" content="Kernel, Silont Project">
+<meta name="author" content="SilonT Project Team">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<head>
+	<title>SilonT Project Web</title>
+</head>
+<link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;700&display=swap" rel="stylesheet">
+<link rel="stylesheet" type="text/css" href="css/index.css">
+<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script type="text/javascript" src="bootstrap/js/bootstrap.js"></script>
+<body>
+	<div class="containerq">
+		<h3 align="center">Welcome To Our Website !</h3>
+		<img src="brand.png">
+			<div class="tombol" align="center">
+				<form action="https://t.me/silont_support">
+					<button class="team">Support Chat</button>
+				</form>
+				<form action="#">
+					<button class="download">Downloads</button>
+				</form>
+				<form action="#">
+					<button class="more">Read More</button>
+				</form>
+			</div>
+	</div>
+	<div class="container-team">
+<!-- 		<div class="row text-center">
+ 			<div class="col-12 col-sm-3" align="center">
+    			<div class="card">
+      				<a href="https://github.com/azrim"><img class="card-img-top img-circle rounded-circle" src="https://avatars.githubusercontent.com/u/51055859?v=4" alt="Card image cap"></a>
+      				<div class="card-block">
+        				<p class="card-text">Lead And Founder SilonT</p>
+      				</div>
+    			</div>
+  			</div>
+  			<div class="col-12 col-sm-3" align="center">
+    			<div class="card">
+      				<a href="https://github.com/azrim"><img class="card-img-top img-circle rounded-circle" src="https://avatars.githubusercontent.com/u/51055859?v=4" alt="Card image cap"></a>
+      				<div class="card-block">
+        				<p class="card-text">Lead And Founder SilonT</p>
+      				</div>
+    			</div>
+  			</div>
+		</div> -->
+	</div>
+</body>
+</html>
