4db6fb19cc5c5a90d91297e4771ba6e8920bf649
Aprianto WPJ <apriantowpj@gmail.com>
2021-05-18 22:24:17 +0700

    change ui

--- index.php
+++ index.php
@@ -14,41 +14,91 @@
 <link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.css">
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 <script type="text/javascript" src="bootstrap/js/bootstrap.js"></script>
+<style type="text/css">
+.containerq{
+	color: #fff;
+}
+.sticky {
+	position: fixed;
+  	top: 0;
+  	width: 100%;
+}
+.sticky + .containerq{
+	padding-top: 50px;
+}
+</style>
 <body>
-	<div class="containerq">
-		<h3 align="center">Welcome To Our Website !</h3>
-		<img src="brand.png">
-			<div class="tombol" align="center">
-				<form action="https://t.me/silont_support">
-					<button class="team">Support Chat</button>
-				</form>
-				<form action="#">
-					<button class="download">Downloads</button>
-				</form>
-				<form action="#">
-					<button class="more">Read More</button>
-				</form>
-			</div>
+	<div class="hero">
+		<div class="hero-inner" align="center">
+			<img src="svg/brand.svg">
+			<form action="#">
+				<button class="download">Downloads</button>
+			</form>
+		</div>
 	</div>
-	<div class="container-team">
-<!-- 		<div class="row text-center">
- 			<div class="col-12 col-sm-3" align="center">
-    			<div class="card">
-      				<a href="https://github.com/azrim"><img class="card-img-top img-circle rounded-circle" src="https://avatars.githubusercontent.com/u/51055859?v=4" alt="Card image cap"></a>
-      				<div class="card-block">
-        				<p class="card-text">Lead And Founder SilonT</p>
-      				</div>
-    			</div>
-  			</div>
-  			<div class="col-12 col-sm-3" align="center">
-    			<div class="card">
-      				<a href="https://github.com/azrim"><img class="card-img-top img-circle rounded-circle" src="https://avatars.githubusercontent.com/u/51055859?v=4" alt="Card image cap"></a>
-      				<div class="card-block">
-        				<p class="card-text">Lead And Founder SilonT</p>
-      				</div>
-    			</div>
-  			</div>
-		</div> -->
-	</div>
+<nav class="navbar navbar-expand-lg navbar-dark" id="navbar">
+  <div class="container">
+    <a class="navbar-brand" href="#">SilonT Project</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div class="navbar-nav ms-auto">
+        <a class="nav-link active" aria-current="page" href="#">Home</a>
+        <a class="nav-link" href="#">Downloads</a>
+      </div>
+    </div>
+  </div>
+</nav>
+<div class="containerq">
+ <div class="containerq col-xxl-8 px-4 py-5">
+    <div class="row flex-lg-row-reverse align-items-center g-5 py-5">
+      <div class="col-sm-8 col-lg-6">
+        <img src="svg/opensource.svg" class="d-block mx-lg-auto img-fluid" alt="Bootstrap Themes" width="700" height="500" loading="lazy">
+      </div>
+      <div class="col-lg-6">
+        <h1 class="display-5 fw-bold lh-1 mb-3">Open Source Code</h1>
+        <p class="lead">Our code is fully open source and you can use it for learn, tracking our work, or etc.</p>
+        <form action="https://github.com/silont-project">
+        	<button class="githab">See Our Code</button>
+        </form>
+      </div>
+    </div>
+  </div>
+ </div>
+ <div class="container-2">
+   <div class="containerq col-xxl-8 px-4 py-5">
+    <div class="row g-5 align-items-center py-5">
+      <div class="col-sm-8 col-lg-6">
+        <img src="svg/opensource.svg" class="d-block mx-lg-auto img-fluid" alt="Bootstrap Themes" width="700" height="500" loading="lazy">
+      </div>
+      <div class="col-lg-6">
+        <h1 class="display-5 fw-bold lh-1 mb-3">Open Source Code</h1>
+        <p class="lead">Our code is fully open source and you can use it for learn, tracking our work, or etc.</p>
+      </div>
+    </div>
+  </div>
+  </div>
+ </div>
+<div class="float">
+	<a href="https://t.me/silont_support"><img src="svg/telegram.svg"></a>
+	<a href="https://www.github.com/silont-project"><img src="svg/github.svg"></a>
+</div>
+<div class="footer">
+	<br>
+	<p>SilonT Project Team<br>
+	Copyright 2021</p>
+</div>
+<script>
+var navbar = document.querySelector("nav");
+var sticky = navbar.offsetTop;
+
+window.onscroll = function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}</script>
 </body>
 </html>
